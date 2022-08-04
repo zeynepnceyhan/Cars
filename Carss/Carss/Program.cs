2402ae3954f59fb64a5026b7a44a293d0df97559
Zeynep Ceyhan <104215922+zeynepnceyhan@users.noreply.github.com>
2022-08-04 11:58:53 +0300

    Cars class
    internship

---
+++ Carss/Carss/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace ConsoleApp5
+{
+    class Program
+    {
+        public class araba
+        {
+            public int KapiSayisi;
+            public string arabaModeli;
+            public string arabaRengi;
+
+
+            public araba(int _KapiSayisi, string _arabaModeli, string _arabaRengi)
+            {
+                KapiSayisi = _KapiSayisi;
+                arabaModeli = _arabaModeli;
+                arabaRengi = _arabaRengi;
+
+            }
+            public void MotorCalistir()
+            {
+                Console.WriteLine("motor çalışıyor!");
+            }
+
+            public void KapilariKilitle()
+            {
+                Console.WriteLine("kapılar kilitleniyor!");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            araba car = new araba(4, "BMW", "Black");
+
+            
+
+            car.MotorCalistir();
+            car.KapilariKilitle();
+
+            Console.WriteLine("Arabanın kapı sayısı: " + car.KapiSayisi);
+            Console.WriteLine("Arabanın modeli: " + car.arabaModeli);
+            Console.WriteLine("Arabanın rengi: " + car.arabaRengi);
+            Console.ReadLine();
+        }
+
+    }
+}
